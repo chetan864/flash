ed89e7a93167d1c97fbc24a9a1a2e882f6cb42a4
chetan <chetan.kakkireni@accenture.com>
2018-07-20 12:36:51 -0700

    test

---
+++ Demo/src/Employee.java
@@ -0,0 +1,15 @@
+
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person p = new Person(7.5,8);
+		//p.sum();
+		
+		System.out.println(p.a);
+		System.out.println(p.b);
+	}
+
+}
---
+++ Demo/src/Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+	
+	int a  ;
+	int b  ;
+	int c ;
+	
+	public Person(double m, int n){
+		a = (int)m;
+		b = n;
+	}
+	
+	
+
+}
